799b2ba1c8528fbe0e0da5dca76370e7c1292e43
Eemeli Saari <saari.eemeli@gmail.com>
2020-07-28 15:57:23 +0300

    Refactor for monad

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,6 @@ import Control.Monad.State
 
 import Topics.Lda as Lda
 import Topics.Vocab
-import Topics.Sampling
-
 
 main :: IO ()
 main = do
@@ -21,14 +19,9 @@ main = do
     let vocab = buildVocab tokens
     let docs = prepareTokens vocab tokens
 
-    let specs = Lda.LdaSpec { ntopics = 2
-                            , alpha = 0.1
-                            , eta = 0.5
-                            , decay = 0.1
-                            , iter = 10
-                            , passes = 5
-                            , nterms = numTerms vocab
-                            }
+    let specs = Lda.ldaSpecDefaults { ntopics = 2, nterms = numTerms vocab }
 
-    let model = Lda.initLda specs in do
-        print (evalState (Lda.update docs 0) model)
+    model <- Lda.initLda specs
+
+    print (evalState (Lda.update docs) model)
+
--- src/Topics/LDA.hs
+++ src/Topics/LDA.hs
@@ -1,77 +1,108 @@
+{-# LANGUAGE DataKinds #-}
+
 module Topics.Lda
 ( LdaSpec(..)
+, ldaSpecDefaults
 , initLda
 , update
 , Lda
 ) where
 
-import Stats.Psi
+import Control.Monad
+import System.Random.MWC
+import System.Random.MWC.Distributions (gamma)
 import Control.Monad.State
 import Data.Matrix as M
-import Data.Vector as V hiding (update, sum, foldl, length, map)
+
+import Stats.Psi
+import Data.Defaults
 
 
-data LdaSpec = LdaSpec 
-    { ntopics :: Int
-    , alpha :: Double
-    , eta :: Double
-    , decay :: Double
-    , iter :: Int
-    , passes :: Int
-    , nterms :: Int
-    , offset :: Double
-    } deriving (Show, Read)
+data LdaSpec a = LdaSpec 
+    { ntopics :: !(Required a Int)
+    , nterms :: !(Required a Int)
+    , alpha :: !(Maybe Double)
+    , eta :: !(Maybe Double)
+    , decay :: !Double
+    , iter :: !Int
+    , passes :: !Int
+    , offset :: !Double
+    , seed :: !Int
+    }
 
-data LdaState = LdaState
-    { _eta :: Vector Double
-    , _alpha :: Vector Double
+
+ldaSpecDefaults :: LdaSpec Defaults
+ldaSpecDefaults = LdaSpec 
+    -- Required parameters
+    { ntopics = ()
+    , nterms = ()
+    -- Optimal parameters
+    , alpha = Nothing
+    , eta = Nothing
+    , decay = 0.1
+    , iter = 10
+    , passes = 5
+    , offset = 1
+    , seed = 42
+    }
+
+
+data Lda = Lda
+    { _eta :: [Double]
+    , _alpha :: [Double]
     , _sstate :: Matrix Double
     , _expElogBeta :: Matrix Double
     , _ndocs :: Int
     , _pass :: Int
     , _offset :: Double
     , _decay :: Double
-    , _nupdate :: Int
+    , _numUpdate :: Int
+    , _maxPasses :: Int
+    , _numTopics :: Int
     } deriving (Show)
 
-data Lda = Lda
-    { spec :: LdaSpec
-    , _state :: LdaState
-    }
 
--- Show only the State
-instance Show Lda where
-    show (Lda _ b) = show b
+type LdaState = State Lda
 
-type LdaMonad = State Lda
+--storeState :: LdaState -> IO Bool
+--storeState s = do
+--    return True
 
-initState :: LdaSpec -> LdaState
-initState s = LdaState
-    { _eta = e
-    , _sstate = st
-    , _ndocs = 0
-    , _pass = 0
-    , _alpha = a
-    , _offset = offset s
-    , _decay = decay s
-    , _nupdate = 0
-    } where
-        e = V.replicate (nterms s) (eta s)
-        a = V.replicate (ntopics s) (alpha s)
-        st = M.zero (ntopics s) (nterms s)
+loadState :: Monad m => String -> m Lda
+loadState p = do
+    return Lda{}
 
-initLda :: LdaSpec -> Lda
-initLda s = Lda
-    { spec = s
-    , _state = initState s
-    }
+initLda :: LdaSpec Complete -> IO Lda
+initLda s = do
+    g <- createSystemRandom
+    st' <- replicateM n $ gamma 100 0.01 g
+    let eeb' = map exp x where x = dirichletExpectation st'
+    return Lda
+        { _eta = replicate (nterms s) (case (eta s) of
+            Just x -> x
+            Nothing -> auto)
+        , _alpha = replicate (ntopics s) (case (alpha s) of
+            Just x -> x
+            Nothing -> auto)
+        , _sstate = M.fromList (ntopics s) (nterms s) st'
+        , _expElogBeta = M.fromList (ntopics s) (nterms s) eeb'
+        , _ndocs = 0
+        , _pass = 0
+        , _numUpdate = 0
+        , _offset = offset s
+        , _decay = decay s
+        , _maxPasses = passes s
+        , _numTopics = ntopics s
+        } where
+            n = (nterms s) * (ntopics s)
+            auto = 1 / fromIntegral (ntopics s)
 
 -- Learning rate
-rho :: LdaState -> Double -> Double
+rho :: Lda -> Double -> Double
 rho st cs = ((_offset st) + ps + (nupd / cs)) ** (-(_decay st)) :: Double
     where
         ps = fromIntegral (_pass st)
-        nupd = fromIntegral (_nupdate st)
+        nupd = fromIntegral (_numUpdate st)
 
 dirichletExpectation :: [Double] -> [Double]
 dirichletExpectation a = map (\x -> (psi x) - y) a
@@ -88,31 +119,23 @@ inferenceStep d = [1.0, 4.5]
 inference :: [[Int]] -> (Matrix Double, Matrix Double)
 inference c = (M.zero 5 3, M.zero 5 3)
 
-eStep :: LdaState -> [[Int]] -> Matrix Double
-eStep st c = gamma
-    where
-        (gamma, sstate) = inference c
-
 --lambda :: LdaState -> Matrix Double
 --lambda st = (_eta st + _sstate st)
 
-updateStep :: [[Int]] -> Lda -> Lda
-updateStep c m = m {_state = newState}
-    where
-        oldState = (_state m)
-        newPass = (_pass oldState) + 1
-        newState = oldState
-            { _pass = newPass
-            }
+updateStep :: Monad m => [[Int]] -> m Lda -> Lda
+updateStep c s = do
+    return s { _pass = newPass , _alpha = (_alpha s)} 
+        where
+            newPass = (_pass s) + 1
 
-updateState :: [[Int]] -> LdaMonad Lda
+updateState :: [[Int]] -> LdaState Lda
 updateState c = state (\st -> let st' = (updateStep c st) in (st', st'))
 
 -- Main loop
-update :: [[Int]] -> LdaMonad Lda
+update :: [[Int]] -> LdaState Lda
 update c = do
     s <- updateState c
-    if ((passes (spec s)) > (_pass (_state s)))
+    if (_maxPasses s) > (_pass s)
         then do 
             update c
     else do 
